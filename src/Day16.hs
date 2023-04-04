d74b45544a29d8310ad44a132b2a0b3874923d11
alex800121 <alex800121@hotmail.com>
2023-04-04 19:43:36 +0800

    Day16 20220423

--- src/Day16.hs
+++ src/Day16.hs
@@ -9,6 +9,7 @@ import Text.Megaparsec.Char
 import Data.List.Split
 import Data.Bifunctor
 import Data.Maybe (fromJust)
+import Debug.Trace
 
 type Valve = String
 type ValveFlow = Map Valve Int
@@ -26,8 +27,20 @@ valveParser = (eof >> pure (Map.empty, Map.empty)) <|> do
   valves <- splitOn ", " <$> some (anySingleBut '\n') <* space
   bimap (Map.insert name n) (Map.insert name (Map.fromList $ map (, 1) valves)) <$> valveParser
 
+buildMap :: ValveMap -> ValveMap -> ValveMap
+buildMap ref acc
+  -- | trace (show acc) False = undefined
+  | acc == acc' = Map.map (Map.filterWithKey (\k _ -> k == "AA" || k `Map.member` acc)) acc
+  | otherwise = buildMap ref acc'
+  where
+    acc' = Map.mapWithKey f acc
+    f k m = Map.delete k . Map.unionWith min m . Map.unionsWith min . Map.mapWithKey g $ m
+    g k a = Map.map (+ a) $ ref Map.! k
+
 day16 :: IO ()
 day16 = do
-  (valveFlow, valveMap) <- fromJust . parseMaybe valveParser <$> readFile "input16.txt"
-  putStrLn $ ("day16a: " ++) $ show (valveFlow, valveMap)
+  (valveFlow, valveMap) <- (\(x, y) -> (Map.filterWithKey (\k a -> a /= 0 || k == "AA") x, buildMap y (Map.filterWithKey (\k _ -> k == "AA" || (x Map.! k /= 0)) y))) . fromJust . parseMaybe valveParser <$> readFile "input16.txt"
+  print valveFlow
+  print valveMap
+  putStrLn $ ("day16a: " ++) $ show ""
   putStrLn $ ("day16b: " ++) $ show ""
