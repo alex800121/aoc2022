96abf41a6155938fd240223d7260196b385c4b3e
alex800121 <alex800121@hotmail.com>
2023-04-05 03:10:25 +0800

    Day16 20230405

--- src/Day16.hs
+++ src/Day16.hs
@@ -4,17 +4,31 @@ module Day16 (day16) where
 import MyLib
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Data.List.Split
 import Data.Bifunctor
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, fromMaybe)
 import Debug.Trace
 
 type Valve = String
 type ValveFlow = Map Valve Int
 type ValveMap = Map Valve (Map Valve Int)
-data GameState = G { accPressure :: Int, time :: Int, closedValves :: ValveFlow, valveMap :: ValveMap }
+data GameState = G { accPressure :: Int, timer :: Int, atValve :: Valve, openPressure :: Int, closedValves :: ValveFlow, valveMap :: ValveMap }
+  deriving (Show, Eq, Ord)
+
+choices :: GameState -> Either GameState (Set GameState)
+choices g@(G accPressure timer atValve openPressure closedValves valveMap)
+  | timer <= 0 = Left g
+  | Set.null nextValves = Left $ G (accPressure + timer * openPressure) 0 atValve openPressure closedValves valveMap
+  | otherwise = undefined
+  where
+    nextValves = undefined
+    f v = let
+      t = undefined
+      in undefined
 
 timeLimitA :: Int
 timeLimitA = 30
@@ -45,7 +59,5 @@ day16 :: IO ()
 day16 = do
   -- (valveFlow, valveMap) <- (\(x, y) -> (Map.filterWithKey (\k a -> a /= 0 || k == "AA") x, buildMap y (Map.filterWithKey (\k _ -> k == "AA" || (x Map.! k /= 0)) y))) . fromJust . parseMaybe valveParser <$> readFile "input16.txt"
   (valveFlow, valveMap) <- (\(x, y) -> (Map.filter (/= 0) x, buildMap y (Map.filterWithKey (\k _ -> k == "AA" || (x Map.! k /= 0)) y))) . fromJust . parseMaybe valveParser <$> readFile "test16.txt"
-  print valveFlow
-  print valveMap
   putStrLn $ ("day16a: " ++) $ show ""
   putStrLn $ ("day16b: " ++) $ show ""
