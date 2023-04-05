dc5cc74d88d4302b8074be13a41802741e5f6caa
alex800121 <alex800121@hotmail.com>
2023-04-05 23:27:08 +0800

    Day18 done

--- src/Day18.hs
+++ src/Day18.hs
@@ -6,8 +6,10 @@ import Data.Function
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Foldable
+import Data.Bifunctor
 
 type V3 n = Vec (S (S (S Z))) n
+type Index = V3 Int
 
 isAdjacent :: Enum a => Vec n a -> Vec n a -> Bool
 isAdjacent x y = manhattan (vZipWith (subtract `on` fromEnum) x y) == 1
@@ -15,11 +17,29 @@ isAdjacent x y = manhattan (vZipWith (subtract `on` fromEnum) x y) == 1
 inBound :: Ord a => (Vec n a, Vec n a) -> Vec n a -> Bool
 inBound (minV, maxV) x = and (vZipWith (<=) minV x) && and (vZipWith (>=) maxV x)
 
+adjacents :: Set Index
+adjacents = Set.fromList $ map (toVec (SS (SS (SS SZ)))) $ [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, -1], [0, 0, 1]] 
+
+flood :: (Index -> Bool) -> Set Index -> Set Index -> Set Index
+flood f acc starts
+  | Set.null starts = acc
+  | otherwise = flood f acc' starts'
+  where
+    acc' = Set.union starts acc
+    starts' = (Set.\\ acc) $ Set.filter f $ Set.unions $ Set.map (\x -> Set.map (vZipWith (+) x) adjacents) starts
+    
+calcSurface :: Index -> Index -> Int
+calcSurface (Cons a (Cons b (Cons c Nil))) (Cons d (Cons e (Cons f Nil))) = 2 * sum [x * y, y * z, z * x]
+  where
+    x = d - a + 1
+    y = e - b + 1
+    z = f - c + 1
+
 day18 :: IO ()
 day18 = do
   input <- map (toVec (SS (SS (SS SZ))) . map (read @Int) . splitOn ",") . lines <$> readFile "input18.txt"
   -- input <- map (toVec (SS (SS (SS SZ))) . map (read @Int) . splitOn ",") . lines <$> readFile "test18.txt"
-  let day18a = (6 * length input) - sum [1 | x <- input, y <- input, isAdjacent x y]
-      minMax = foldl' (\(x, y) z -> (vZipWith min x z, vZipWith max y z)) (pure maxBound, pure minBound) input
-  putStrLn $ ("day18a: " ++) $ show day18a
-  putStrLn $ ("day18b: " ++) $ show minMax
+  let day18a n = (6 * length n) - sum [1 | x <- n, y <- n, isAdjacent x y]
+      minMax = bimap (subtract 1) (+ 1) $ foldl' (\(x, y) z -> (vZipWith min x z, vZipWith max y z)) (pure maxBound, pure minBound) input
+  putStrLn $ ("day18a: " ++) $ show $ day18a input
+  putStrLn $ ("day18b: " ++) $ show $ subtract (uncurry calcSurface minMax) $ day18a $ Set.toList $ flood (\x -> inBound minMax x && x `Set.notMember` Set.fromList input) Set.empty (Set.singleton (fst minMax))
--- src/Day19.hs
+++ src/Day19.hs
@@ -1,30 +1,8 @@
 module Day19 (day19) where
 
-import MyLib hiding (Nat (..))
-import Data.Sequence
-import qualified Data.Sequence as Seq
-import Debug.Trace
-
-input :: Int
-input = 3018458
-
-day19a :: Seq x -> Seq x -> x
-day19a (x :<| Empty) Empty = x
-day19a acc Empty = day19a Empty acc
-day19a acc (x :<| Empty) = day19a Empty (x :<| acc)
-day19a acc (x :<| y :<| xs) = day19a (acc :|> x) xs
-
-day19b :: Show x => Seq x -> x
--- day19b xs | trace (show xs) False = undefined
-day19b (x :<| Empty) = x
-day19b (x :<| xs) = day19b (xs' :|> x)
-  where
-    l = Seq.length xs + 1
-    n = l `div` 2
-    xs' = deleteAt (n - 1) xs
-
+import MyLib
 
 day19 :: IO ()
 day19 = do
-  putStrLn $ ("day19a: " ++) $ show $ day19a Empty $ fromList [1..input]
-  putStrLn $ ("day19b: " ++) $ show $ day19b $ fromList [1..input] 
+  putStrLn $ ("day19a: " ++) $ show ""
+  putStrLn $ ("day19b: " ++) $ show ""
