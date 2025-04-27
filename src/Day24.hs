59c585887459ba14a3cf7a3aef0b4042759fba4b
alex800121 <alex800121@hotmail.com>
2025-04-27 23:38:12 +0800

    fast day24

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,76 +1,93 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Day24 (day24) where
 
+import Data.Array.Unboxed
+import Data.Bits
+import Data.Word
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IS
+import Data.List (foldl')
+import Data.PQueue.Prio.Min qualified as Q
+import Debug.Trace (traceShow)
 import Paths_AOC2022
-import Data.Array
-import Data.Function
-import Data.List
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Debug.Trace
-import MyLib
-
-type Blizzard = Array Index Char
-
-type Index = (Int, Int)
-
-type Bound = (Index, Index)
-
-data GameState = G {time :: Int, start :: Index, end :: [Index]}
-  deriving (Show, Eq)
-
-instance Ord GameState where
-  compare (G t1 s1 e1) (G t2 s2 e2) = compare (t1 + m1) (t2 + m2) <> compare t1 t2 <> compare m1 m2 <> compare s1 s2 <> compare e1 e2
-    where
-      f _ [] = 0
-      f a (x : y) = manhattan' a x + f x y
-      m1 = f s1 e1
-      m2 = f s2 e2
+import Data.Vector.Unboxed (Vector)
+import Data.Vector.Unboxed qualified as V
+import Data.Vector.Unboxed.Mutable (STVector)
+import Data.Vector.Unboxed.Mutable qualified as M
 
 manhattan' (a, b) (c, d) = abs (a - c) + abs (b - d)
 
-inputParser :: String -> Blizzard
-inputParser = (\x -> listArray ((0, 0), (length (head x) - 1, length x - 1)) (concat $ transpose x)) . map (init . tail) . init . tail . lines
+-- width = 120
+-- height = 25
 
-adjacents :: Set Index
-adjacents = Set.fromList [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
-
-checkAvail :: Bound -> Blizzard -> Int -> Index -> Bool
-checkAvail b@((minx, miny), upperBound@(maxx, maxy)) blizzard minute a@(x, y) =
-  a == (minx, miny - 1) || a == (maxx, maxy + 1) || (withinBound b a && all f "<>^v")
+inputParser st = bl
   where
+    st' = lines st
+    b@((minx, miny), (maxx, maxy)) = ((1, 1), (length (head st') - 2, length st' - 2))
+    lx = maxx - minx + 1
+    ly = maxy - miny + 1
+    bl :: Array (Int, Int) Char =
+      array
+        b
+        [ ((x, y), c)
+          | (y, l) <- zip [0 ..] st',
+            (x, c) <- zip [0 ..] l,
+            inRange b (x, y)
+        ]
+
+adjacents :: [Int]
+adjacents = map toInt [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
+
+checkAvail blizzard minute i =
+  (x, y) == start || (x, y) == end || (inRange b a && f x y minute)
+  where
+    b@((minx, miny), (maxx, maxy)) = bounds blizzard
+    start = (minx, miny - 1)
+    end = (maxx, maxy + 1)
+    a@(x, y) = fromInt i
     width = maxx - minx + 1
     height = maxy - miny + 1
-    f c = case c of
-      '>' -> (blizzard ! ((x - minute) `mod` width, y)) /= '>'
-      '<' -> (blizzard ! ((x + minute) `mod` width, y)) /= '<'
-      'v' -> (blizzard ! (x, (y - minute) `mod` height)) /= 'v'
-      '^' -> (blizzard ! (x, (y + minute) `mod` height)) /= '^'
+    f x y t =
+      and
+        [ blizzard ! ((x - t - minx) `mod` width + minx, y) /= '>',
+          blizzard ! ((x + t - minx) `mod` width + minx, y) /= '<',
+          blizzard ! (x, (y - t - miny) `mod` height + miny) /= 'v',
+          blizzard ! (x, (y + t - miny) `mod` height + miny) /= '^'
+        ]
 
-withinBound :: Bound -> Index -> Bool
-withinBound ((a, b), (c, d)) (x, y) = x >= a && y >= b && x <= c && y <= d
-
-choices :: Bound -> Blizzard -> Int -> Index -> Set Index
-choices bound blizzard minute i = Set.filter (checkAvail bound blizzard minute) $ Set.map (+& i) adjacents
-
-aStar :: Bound -> Blizzard -> Set GameState -> GameState
-aStar bound blizzard s
-  | null e = picked
-  | head e == st = aStar bound blizzard $ Set.insert (G t (head e) (tail e)) rest
-  | otherwise = aStar bound blizzard nextGs
+fromInt i = (x, y)
   where
-    (picked@(G t st e), rest) = Set.deleteFindMin s
-    nexts = choices bound blizzard (t + 1) st
-    nextGs = Set.union rest $ Set.map (\x -> G (t + 1) x e) nexts
+    x = i `shiftR` 5
+    y = i `mod` (2 ^ 5)
+
+toInt (x, y) = (x `shiftL` 5) + y
+
+bfs bound blizzard ends !s !t
+  | IS.null s = Nothing
+  | null ends = Just t
+  | e : es <- ends, IS.foldr (\x acc -> acc || e == x) False s = bfs bound blizzard es (IS.singleton e) t
+  | otherwise = bfs bound blizzard ends s' (t + 1)
+  where
+    s' = IS.foldl' f IS.empty s
+    f acc x = foldl' (g x) acc adjacents
+    g x acc' y
+      | checkAvail blizzard (t + 1) i = IS.insert i acc'
+      | otherwise = acc'
+      where
+        i = x + y
+
+solve b bl ends s = bfs b bl ends (IS.singleton s) 0
 
 day24 :: IO ()
 day24 = do
-  -- blizzard <- inputParser <$> readFile "test24.txt"
-  blizzard <- inputParser <$>(getDataDir >>= readFile . (++ "/input/input24.txt")) 
+  blizzard <- inputParser <$> (getDataDir >>= readFile . (++ "/input/input24.txt"))
   let bound@((mx, my), (w, h)) = bounds blizzard
-      start = (mx, my - 1)
-      end = (w, h + 1)
-  -- print blizzard
-  putStrLn $ ("day24a: " ++) $ show $ time $ aStar bound blizzard (Set.singleton (G 0 start [end]))
-  putStrLn $ ("day24b: " ++) $ show $ time $ aStar bound blizzard (Set.singleton (G 0 start [end, start, end]))
+      start = toInt (mx, my - 1)
+      end = toInt (w, h + 1)
+  putStrLn
+    . ("day24a: " ++)
+    . show
+    $ solve bound blizzard [end] start
+  putStrLn
+    . ("day24b: " ++)
+    . show
+    $ solve bound blizzard [end, start, end] start
