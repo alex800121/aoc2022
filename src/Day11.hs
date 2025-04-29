72ff1cb304a4619c8813fd61df7978c50cb38524
alex800121 <alex800121@hotmail.com>
2025-04-29 19:36:03 +0300

    fast day11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,9 +1,13 @@
 module Day11 (day11) where
 
 import Data.Char
+import Data.Foldable (foldl')
 import Data.List
 import Data.List.Split
 import Data.Maybe (fromJust)
+import Data.Ord (Down (Down), comparing)
+import Data.Sequence (Seq (..))
+import Data.Sequence qualified as S
 import Data.Vector (Vector)
 import Data.Vector qualified as V
 import Data.Vector.Mutable qualified as MV
@@ -13,51 +17,72 @@ import Paths_AOC2022
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-data Monkey = M {items :: [Integer], operation :: Integer -> Integer, test :: Integer, ifTrue :: Integer, ifFalse :: Integer, inspected :: Integer}
+data Monkey = M {items :: [Int], operation :: Int -> Int, test :: Int, ifTrue :: Int, ifFalse :: Int, inspected :: Int}
 
 type Monkeys = Vector Monkey
 
 instance Show Monkey where
   show x = show (items x, ifTrue x, ifFalse x)
 
-monkeyInspects :: (Integer -> Integer) -> Monkeys -> Integer -> Monkeys
--- monkeyInspects x ms k | trace (show ms) False = undefined
+monkeyInspects :: (Int -> Int) -> Monkeys -> Int -> Monkeys
 monkeyInspects x ms k = ms'
   where
-    m = ms V.! fromIntegral k
-    (t, f) = partition ((== 0) . (`mod` test m)) $ map (x . operation m) $ items m
-    m' = m {items = [], inspected = fromIntegral (length (items m)) + inspected m}
-    ms' =
-      V.modify (\v -> MV.write v (fromIntegral k) m') $
-        V.modify (\v -> MV.modify v (\n -> n {items = t ++ items n}) (fromIntegral $ ifTrue m)) $
-          V.modify (\v -> MV.modify v (\n -> n {items = f ++ items n}) (fromIntegral $ ifFalse m)) ms
+    m = ms V.! k
+    m' = m {items = [], inspected = length (items m) + inspected m}
+    ms' = foldl' f (V.modify (\v -> MV.write v k m') ms) (x . operation m <$> items m)
+    f acc n
+      | n `mod` test m == 0 = V.modify (\v -> MV.modify v (\x -> x {items = n : items x}) (ifTrue m)) acc
+      | otherwise = V.modify (\v -> MV.modify v (\x -> x {items = n : items x}) (ifFalse m)) acc
 
 monkeysParser :: Parser [(Int, Monkey)]
 monkeysParser =
   (eof >> pure []) <|> do
     n <- string "Monkey " >> signedInteger <* char ':' <* space
-    items <- map (read @Integer) . splitOn ", " <$> (string "Starting items: " >> some (anySingleBut '\n') <* space)
+    items <- map (read @Int) . splitOn ", " <$> (string "Starting items: " >> some (anySingleBut '\n') <* space)
     operation <- string "Operation: new = " >> opParser
-    test <- string "Test: divisible by " >> fromIntegral <$> signedInteger <* space
-    ifTrue <- string "If true: throw to monkey " >> fromIntegral <$> signedInteger <* space
-    ifFalse <- string "If false: throw to monkey " >> fromIntegral <$> signedInteger <* space
+    test <- string "Test: divisible by " >> signedInteger <* space
+    ifTrue <- string "If true: throw to monkey " >> signedInteger <* space
+    ifFalse <- string "If false: throw to monkey " >> signedInteger <* space
     ((n, M items operation test ifTrue ifFalse 0) :) <$> monkeysParser
 
-opParser :: Parser (Integer -> Integer)
+opParser :: Parser (Int -> Int)
 opParser = do
-  a <- (const . fromIntegral <$> signedInteger) <|> (some (anySingleBut ' ') >> pure id)
+  a <- const <$> signedInteger <|> (some (anySingleBut ' ') >> pure id)
   space
   x <- (char '+' >> pure (+)) <|> (char '*' >> pure (*))
   space
-  b <- (const . fromIntegral <$> signedInteger) <|> (some (satisfy (not . isSpace)) >> pure id)
+  b <- const <$> signedInteger <|> (some (satisfy (not . isSpace)) >> pure id)
   space
   pure $ \y -> x (a y) (b y)
 
 day11 :: IO ()
 day11 = do
   -- monkeys <- (\x -> V.replicate (length x) undefined V.// x) . fromJust . parseMaybe monkeysParser <$> readFile "test11.txt"
-  monkeys <- (\x -> V.replicate (length x) undefined V.// x) . fromJust . parseMaybe monkeysParser <$> (getDataDir >>= readFile . (++ "/input/input11.txt"))
-  let test' = V.foldl1' lcm $ V.map test monkeys
-  -- print test'
-  putStrLn $ ("day11a: " ++) $ show $ product $ take 2 $ reverse $ sort $ map inspected $ V.toList $ (!! 20) $ iterate (\x -> foldl' (monkeyInspects (`div` 3)) x [0 .. (fromIntegral (length x) - 1)]) monkeys
-  putStrLn $ ("day11b: " ++) $ show $ product $ take 2 $ reverse $ sort $ map inspected $ V.toList $ (!! 10000) $ iterate (\x -> foldl' (monkeyInspects (`mod` test')) x [0 .. (fromIntegral (length x) - 1)]) monkeys
+  monkeys <-
+    (\x -> V.replicate (length x) undefined V.// x)
+      . fromJust
+      . parseMaybe monkeysParser
+      <$> (getDataDir >>= readFile . (++ "/input/input11.txt"))
+  let cm = V.foldl1' lcm $ V.map test monkeys
+  putStrLn
+    . ("day11a: " ++)
+    . show
+    . product
+    . take 2
+    . sortBy (comparing Down)
+    . map inspected
+    . V.toList
+    . (!! 20)
+    $ iterate (\x -> foldl' (monkeyInspects (`div` 3)) x [0 .. (length x - 1)]) monkeys
+  putStrLn
+    . ("day11b: " ++)
+    . show
+    . product
+    . take 2
+    $ sortBy
+      (comparing Down)
+      ( map inspected
+          . V.toList
+          . (!! 10000)
+          $ iterate (\x -> foldl' (monkeyInspects (`mod` cm)) x [0 .. (length x - 1)]) monkeys
+      )
