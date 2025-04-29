af74511e071eae16729c02f3059ad6febf8024fc
alex800121 <alex800121@hotmail.com>
2025-04-29 12:41:29 +0300

    fast day9

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,24 +1,29 @@
 module Day9 (day9) where
 
-import Paths_AOC2022
-import MyLib
 import Data.List
+import Data.Set (Set)
+import Data.Set qualified as Set
+import MyLib
+import Paths_AOC2022
 
-data Dir = R
-         | L
-         | U
-         | D
+data Dir
+  = R
+  | L
+  | U
+  | D
   deriving (Show, Read, Eq, Ord)
+
 type Index = (Int, Int)
+
 type Rope = [Index]
+
 type Ins = (Dir, Int)
 
-
-step :: Rope -> Ins -> Rope
-step [] _ = []
-step (x : xs) (d, n)
-  | n <= 0 = x : xs
-  | otherwise = step (xs `follow` move x d) (d, n - 1)
+step :: (Set Index, Rope) -> Ins -> (Set Index, Rope)
+step (s, []) _ = (s, [])
+step (s, x : xs) (d, n)
+  | n <= 0 = (s, x : xs)
+  | otherwise = step ((s, xs, id) `follow` move x d) (d, n - 1)
 
 move :: Index -> Dir -> Index
 move i d = i +& i'
@@ -29,9 +34,9 @@ move i d = i +& i'
       U -> (0, 1)
       D -> (0, -1)
 
-follow :: [Index] -> Index -> [Index]
-follow [] x = [x]
-follow (y : ys) x = x : follow ys y'
+-- follow :: Rope -> Index -> Rope
+follow (s, [], f) x = (Set.insert x s, f [x])
+follow (s, y : ys, f) x = follow (s, ys, f . (x :)) y'
   where
     y' = y `follows` x
 
@@ -41,11 +46,10 @@ follows y@(a, b) x@(c, d)
   | otherwise = y +& (signum (c - a), signum (d - b))
 
 surrounds :: Index -> Index -> Bool
-surrounds x y = x `elem`  [y +& (a, b) | a <- [(-1)..1], b <- [(-1)..1]]
+surrounds x y = x `elem` [y +& (a, b) | a <- [(-1) .. 1], b <- [(-1) .. 1]]
 
-  
 day9 :: IO ()
 day9 = do
-  input <- concatMap ((\(x, y) -> replicate y (x, 1)) . (\(x : y : _) -> (read @Dir x, read @Int y)) . words) . lines <$> (getDataDir >>= readFile . (++ "/input/input9.txt")) 
-  putStrLn $ ("day9a: " ++) $ show $ length $ nub $ map last $ scanl' step (replicate 2 (0, 0)) input
-  putStrLn $ ("day9b: " ++) $ show $ length $ nub $ map last $ scanl' step (replicate 10 (0, 0)) input
+  input <- concatMap ((\(x, y) -> replicate y (x, 1)) . (\(x : y : _) -> (read @Dir x, read @Int y)) . words) . lines <$> (getDataDir >>= readFile . (++ "/input/input9.txt"))
+  putStrLn $ ("day9a: " ++) $ show $ length $ fst $ foldl' step (Set.empty, replicate 2 (0, 0)) input
+  putStrLn $ ("day9b: " ++) $ show $ length $ fst $ foldl' step (Set.empty, replicate 10 (0, 0)) input
